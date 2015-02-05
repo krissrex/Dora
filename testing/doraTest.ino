f46c7e62387e4d39f3b36a389e4f52783b7353fb
Kristian Rekstad <krirek@msn.com>
2015-02-05 14:34:04 +0100

    La til rotasjon

--- testing/doraTest.ino
+++ testing/doraTest.ino
@@ -1,6 +1,23 @@
 
+#include "PLab_ZumoMotors.h"
+#include "Pushbutton.h"
+#include "QTRSensors.h"
+#include "ZumoBuzzer.h"
+#include "ZumoMotors.h"
+#include "ZumoReflectanceSensorArray.h"
+
+
+#define MAX_SPEED 400
+#define SPEED 200
+
+
+Pushbutton zumo_button(ZUMO_BUTTON);
+ZumoMotors motors;
+
+
 void setup(){
-	
+	zumo_button.waitForButton();
+	motors.setSpeeds(-SPEED, SPEED);
 }
 
 void loop(){
