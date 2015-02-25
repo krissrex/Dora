68ae5d27ed6191748fca47c07ee6cedd1b73b11b
Kristian Rekstad <krirek@msn.com>
2015-02-25 13:13:59 +0100

    asd

--- autonom/Dora_found.cpp
+++ autonom/Dora_found.cpp
@@ -4,26 +4,31 @@
 #define TURNSPEED 150 
 void Dora::found()
 {
+  motion.update();
+
   usSensor1.getDistance();
   usSensor2.getDistance();
   Direction d;
   if(usSensor1.getLastDistance()==0||(usSensor1.getLastDistance()>usSensor2.getLastDistance()))
+  {
     d=RIGHT;
-  else
+  }
+  else 
+  {
     d=LEFT;
-
+  }
 
   lastSeen=d;
-if(usSensor1.getLastDistance()==0||usSensor2.getLastDistance()==0))
-{
-motion.turnWithSpeed(d,0.5, TURNSPEED,TIMEOUT);
-return;
-}
+
+  if(usSensor1.getLastDistance()==0||usSensor2.getLastDistance()==0)
+  {
+    motion.turnWithSpeed(d,0.5, TURNSPEED,TIMEOUT);
+    return;
+  }
 
   if(d==RIGHT)
   {
-       motion.turnWithSpeed(d,usSensor2.getLastDistance()/usSensor1.getLastDistance(), TURNSPEED,TIMEOUT);
-       
+       motion.turnWithSpeed(d,usSensor2.getLastDistance()/usSensor1.getLastDistance(), TURNSPEED,TIMEOUT);  
   }
   else
   {
